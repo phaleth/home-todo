ea0b09001455ca49499699ffc0078d9d4dac1af6
phaleth <29838606+phaleth@users.noreply.github.com>
2022-08-01 16:57:42 +0200

    Rename images and add some styles to App

--- components/app.tsx
+++ components/app.tsx
@@ -2,8 +2,31 @@ import styles from './app.module.scss';
 
 export default () => {
   return (
-    <div className={styles.container}>
-      <h1>Hello World!</h1>
-    </div>
+    <main className={styles.container}>
+      <section className={styles.input}>
+        <img src='images/checklist.png' />
+        <input type="text" />
+      </section>
+      <section className={styles.controls}>
+        <section className={styles.dblCheckComplete}>
+          <img src='images/double-checkmark.png' />
+          <button type="button" className={styles.complete}>Complete all tasks</button>
+        </section>
+        <button type="button" className={styles.clear}>Clear completed</button>
+      </section>
+      <hr />
+      <ul className={styles.list}>
+        <li>
+          <img src='images/circle-empty.png' />
+          Text
+          <img src='images/circle-cancel.png' />
+        </li>
+      </ul>
+      <hr />
+      <section className={styles.statusBar}>
+        <span><strong className={styles.count}>3</strong> tasks left</span>
+        <nav className={styles.filter}><span className={styles.selectedFilter}>All</span><span>Incomplete</span><span>Complete</span></nav>
+      </section>
+    </main>
   )
 }
