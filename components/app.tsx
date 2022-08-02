04119c4e2f6bb7c0a69ab84a3a6d0d42019b167c
phaleth <29838606+phaleth@users.noreply.github.com>
2022-08-02 15:58:47 +0200

    Componentize more and make list items clickable

--- components/app.tsx
+++ components/app.tsx
@@ -2,6 +2,8 @@ import { useState } from 'react';
 import styles from './app.module.scss';
 import NewTodo from './new-todo';
 import ListControls from './list-controls';
+import TodoList from './todo-list';
+import StatusBar from './status-bar';
 
 export default (): JSX.Element => {
   const [items, setItems] = useState([
@@ -14,30 +16,9 @@ export default (): JSX.Element => {
       <NewTodo />
       <ListControls />
       <hr />
-      <ul className={styles.list}>
-        {items.map(item => (
-          <li key={item.text} className={styles.item}>
-            <img className={item.done ? styles.radioBtnChecked : styles.radioBtnEmpty}
-              src={item.done ? 'images/circle-checkmark.png' : 'images/circle-empty.png'}
-            />
-            <span className={styles.text}>{item.text}</span>
-            <img className={`${styles.delBtn} ${item.done ? '' : styles.hidden}`}
-              src='images/circle-cancel.png'
-            />
-          </li>
-        ))}
-      </ul>
+      <TodoList items={items} setItems={setItems} />
       <hr />
-      <section className={styles.statusBar}>
-        <span>
-          <strong className={styles.count}>3</strong> tasks left
-        </span>
-        <nav className={styles.filter}>
-          <span className={styles.selectedFilter}>All</span>
-          <span>Incomplete</span>
-          <span>Complete</span>
-        </nav>
-      </section>
+      <StatusBar />
     </main>
   )
 }
