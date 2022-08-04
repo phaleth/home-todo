5ba54666aa225bae3d06d6d3518288d635a4339c
phaleth <29838606+phaleth@users.noreply.github.com>
2022-08-04 16:48:33 +0200

    Add more validations of a new todo item

--- services/todo.service.ts
+++ services/todo.service.ts
@@ -6,12 +6,16 @@ export const addTodo = (evt, items, setItems, filter, setFilteredItems) => {
   if (!todoTxt) {
     alert('Item text is not defined');
   } else if (
-    items.find(item => item.text.toLowerCase() === todoTxt.toLowerCase())
+    items.find((item) => item.text.toLowerCase() === todoTxt.toLowerCase())
   ) {
     alert('Item already exists');
+  } else if (items.length == 10) {
+    alert('List of items is already at maximum capacity of 10');
+  } else if (todoTxt.length > 40) {
+    alert('Item text cannot have more than 40 characters');
   } else {
     todoTxtEl.value = '';
-    items.push({ done: false, text: todoTxt });
+    items.push({ done: false, text: todoTxt.trim() });
     setItems([...items]);
     filterItems(filter, items, setFilteredItems);
   }
