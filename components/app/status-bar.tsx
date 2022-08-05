e9f0f5d34773fc5e04c4a479cada2a6b188e28a7
phaleth <29838606+phaleth@users.noreply.github.com>
2022-08-05 12:07:36 +0200

    Cleanup StatusBar component

--- components/app/status-bar.tsx
+++ components/app/status-bar.tsx
@@ -1,12 +1,8 @@
-import { useContext } from 'react';
 import styles from './status-bar.module.scss';
-import { ItemsContext } from '../app';
 import Filter from './status-bar/filter-nav';
 import TaskCounter from './status-bar/task-counter';
 
 export default ({ setFilter, setFilteredItems }): JSX.Element => {
-  const items = useContext(ItemsContext);
-
   return (
     <section className={styles.statusBar}>
       <TaskCounter />
